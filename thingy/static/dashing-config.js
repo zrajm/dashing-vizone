e7de0c595408c6e09c0a56469941125ca454bee6
zrajm <zrajm@klingonska.org>
2015-11-04 23:27:51 +0100

    Now updates clock using time from clock/ view.

--- thingy/static/dashing-config.js
+++ thingy/static/dashing-config.js
@@ -2,7 +2,52 @@
 
 var dashboard = new Dashboard();
 
-dashboard.addWidget('clock_widget', 'Clock');
+// Invoked by clock_widget's getData() function, return object with 'date' +
+// 'time' attributes. These values are kept up-to-date with repeated AJAX
+// requests to a Django view, which output the server time.
+var current_datetime = (function () {
+    var time = 0, date = 0, old_datetime = "";
+
+    // Update internal date and time + tell widget to update.
+    function updateTime(datetime) {
+        date = datetime[0];
+        time = datetime[1];
+        dashboard.publish('clock_widget/getData');
+    }
+
+    function ajax(url, callback) {
+        var req = new XMLHttpRequest();
+        req.addEventListener("load", function (event) {
+            callback(event.target);
+        });
+        req.open('GET', encodeURI(url));
+        req.send();
+    }
+
+    // Fetch new time in background, update widget if new value gotten.
+    function fetchTime() {
+        ajax('/clock', function (req) {
+            var x, datetime = req.responseText.replace(/[.].*/, "");
+            if (datetime !== old_datetime) {
+                old_datetime = datetime;
+                updateTime(datetime.split(" "));
+            }
+        });
+    }
+    setInterval(fetchTime, 500);              // repeat twice a second
+    return function () {
+        return { date: date, time: time };
+    };
+}());
+
+dashboard.addWidget('clock_widget', 'Clock', {
+    interval: 0,                               // turn off automatic update
+    getData: function () {
+        // When "dashboard.publish('clock_widget/getData')" is called, update
+        // widget values with whatever `current_datetime()` returns.
+        $.extend(this.scope, current_datetime());
+    }
+});
 
 dashboard.addWidget('current_valuation_widget', 'Number', {
     getData: function () {
