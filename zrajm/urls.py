86a36019a66ba67e0634d3b22441d46ba470c3b7
zrajm <zrajm@klingonska.org>
2015-11-04 19:15:42 +0100

    Configured Dashing

--- zrajm/urls.py
+++ zrajm/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from dashing.utils import router
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include(router.urls)),
 ]
